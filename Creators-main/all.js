7f4949a1f70e3de536f9918a87b88f373bd51fb3
LucasAlmeida-jpg <lucasalmeidagouvea123@gmail.com>
2022-07-26 10:42:06 -0300

    push

---
+++ Creators-main/all.js
@@ -0,0 +1,23 @@
+document.querySelectorAll('.accordion__button').forEach(button => {
+    button.addEventListener('click', () => {
+        const accordionContent = button.nextElementSibling
+
+        button.classList.toggle('accordion__button--active')
+
+        if(button.classList.contains('accordion__button--active')) {
+            accordionContent.style.maxHeight = accordionContent.scrollHeight + 'px'
+        }
+        else {
+            accordionContent.style.maxHeight = 0
+        }
+    })  
+})
+
+$(window).scroll(function() {
+    var math = $(window).scrollTop() / 750% Math.PI;
+    $('#leftgear').css({ transform: 'rotate(' + math + 'rad)' });
+    $('#rightgear').css({ transform: 'rotate(-' + math + 'rad)' });
+    $('#leftge').css({ transform: 'rotate(-' + math + 'rad)' });
+    $('#rightg').css({ transform: 'rotate(-' + math + 'rad)' });
+    $('#right').css({ transform: 'rotate(' + math + 'rad)' });
+    });
