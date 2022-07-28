a610c03eb01ae7586c123891e865832ae4445082
LucasAlmeida-jpg <lucasalmeidagouvea123@gmail.com>
2022-07-28 16:35:35 -0300

    atualizacao navbar

--- all.js
+++ all.js
@@ -24,7 +24,7 @@ $(window).scroll(function() {
 
 
     const toggleButton = document.getElementsByClassName('toggle-button') [0]
-    const navbarLinks = document.getElementsByClassName('navbar-links') [0]
+    const navbar = document.getElementsByClassName('navbar') [0]
     toggleButton.addEventListener('click', () => {
-        navbarLinks.classList.toggle('active')
+        navbar.classList.toggle('active')
     })
